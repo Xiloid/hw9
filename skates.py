501d57e4694933ea4763a7ebc6a8d49e1a1eed05
Xiloid <faustov.roman.a@gmail.com>
2021-02-27 18:27:56 +0200

    commited

--- skates.py
+++ skates.py
@@ -16,9 +16,44 @@
     [37, 38, 39] , [37, 37, 37, 37] -> 3
     Напишите несколько тестов
 """
+available_sizes = [39, 38, 41, 37]
+foot_sizes = [40, 39, 41]
 
 
+def main():
+    # для ввода вручную:
+    # available_sizes = sorted(list(map(int, input('Введите доступные размеры коньков через проблел: ').split())))
+    # foot_sizes = sorted(list(map(int, input('Введите размеры ног посетителей через проблел: ').split())))
+    result = skates(available_sizes, foot_sizes)
+    print(result)
+    error_test()
 
 
-#def skates(available_sizes, foot_sizes):
-#    pass
+def skates(available_sizes, foot_sizes):
+    result = 0
+    while True:
+        try:
+            available_sizes = list(filter(lambda x: x >= min(foot_sizes), available_sizes))
+            available_sizes.pop(0)
+            foot_sizes.pop(0)
+            result += 1
+        except:
+            break
+    return result
+
+
+def error_test():
+    available_sizes2 = [37, 38, 39, 40]
+    foot_sizes2 = [37, 37, 40, 40]
+    available_sizes3 = [37, 38, 39, 40]
+    foot_sizes3 = [42]
+    available_sizes4 = [37, 38, 39]
+    foot_sizes4 = [37, 37, 37, 37]
+    assert skates(available_sizes2, foot_sizes2) == 3
+    assert skates(available_sizes3, foot_sizes3) == 0
+    assert skates(available_sizes4, foot_sizes4) == 3
+    print('No assertion errors')
+
+
+if __name__ == "__main__":
+    main()
