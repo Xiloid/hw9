fcad9b0ab00f5d0ae51baf01d2715539369483ff
Xiloid <faustov.roman.a@gmail.com>
2021-02-28 16:35:52 +0200

    commited

--- slow_decorator.py
+++ slow_decorator.py
@@ -7,3 +7,31 @@
     2. @slow_decorator - замедляет выполнение функции на 5 секунд
     Используйте библиотеку time, а именно функции time и sleep
 """
+import time
+
+
+def time_decorator(func):
+    def wrapped(*args):
+        start_time = time.time()
+        res = func(*args)
+        print(time.time() - start_time)
+        return res
+    return wrapped
+
+
+def slow_decorator(func):
+    def wrapped(*args):
+        time.sleep(5)
+        res = func(*args)
+        return res
+    return wrapped
+
+
+@slow_decorator
+@time_decorator
+def func(first, second):
+    result = bin(int(first, 2) + int(second, 2))
+    return result
+
+
+print(func("111", "0000"))
